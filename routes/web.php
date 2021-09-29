836bc1378197b93ec5efacd40c241151510110b5
teewhy <hamendment@gmail.com>
2021-09-29 14:37:39 +0100

    web route

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ApplicationController;
 
 
 
 
-Route::get('/', function () {
-    return view('auth/login');
+
+Route::get('forgot/password',[ApplicationController::class,'forgot_password'])->name('forgot.password');
+Route::post('auth/check',[AuthController::class,'auth_check'])->name('auth.check');
+Route::get('logout',[AuthController::class,'logout'])->name('logout');
+Route::post('save/account/form',[AuthController::class,'save_new_account'])->name('save.account.form');
+
+
+Route::group(['middleware'=>['authcheck']], function() {
+    Route::get('get/account/form',[AuthController::class,'register_form'])->name('get.account.form');
+    Route::get('/',[AuthController::class,'auth_login'])->name('auth.login');
+    Route::get('dashboard',[AuthController::class,'applicant_dashboard'])->name('applicant.dashboard');
+   
 });
 
-Route::get('/register', function () {
-    return view('/auth/register');
-});
-Route::get('/forgot', function () {
-    return view('/auth/forgot-password');
-});
-Route::get('/home', function () {
-    return view('/pages/home');
-});
+
+
 Route::get('/create_application', function () {
     return view('/pages/create_application');
 });
