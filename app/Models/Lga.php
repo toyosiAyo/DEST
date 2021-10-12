019d3a24adff365b42f9f2e080cca4537a28c632
hameddavid <reganalyst@yahoo.com>
2021-10-12 16:41:37 +0100

    Country State lag

--- app/Models/Lga.php
+++ app/Models/Lga.php
@@ -5,9 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Applicant extends Model
+class Lga extends Model
 {
     use HasFactory;
-
-    protected $table="applicants";
+    protected $table="lga";
 }
