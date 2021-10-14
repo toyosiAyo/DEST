4adda96f7d7f8eab297db6c90e08c2c25c195097
toyxy <toyxyboy@gmail.com>
2021-10-14 21:20:57 +0100

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Applicant;
 use App\Models\Application;
 use App\Models\ApplicantPayment;
+use Illuminate\Support\Facades\Validator;
 
 class ApplicationController extends Controller
 {
