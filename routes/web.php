ec55c8e388c3d2e59220b37328bc84beb5eb4d47
toyxy <toyxyboy@gmail.com>
2021-10-23 10:16:37 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,18 +26,19 @@ Route::group(['middleware'=>['authcheck']], function() {
     Route::get('dashboard',[AuthController::class,'applicant_dashboard'])->name('applicant.dashboard');
 
 
-Route::post('save/app/form',[ApplicationController::class,'save_app_form'])->name('save.app.form');
-Route::get('app_form',[ApplicationController::class,'get_app_form'])->name('app.form');
-Route::get('create_application',[ApplicationController::class,'create_application'])->name('create.application');
+    Route::post('save/app/form',[ApplicationController::class,'save_app_form'])->name('save.app.form');
+    Route::get('app_form',[ApplicationController::class,'get_app_form'])->name('app.form');
+    Route::get('create_application',[ApplicationController::class,'create_application'])->name('create.application');
 
 
-Route::get('get_remita_config',[RemitaConfig::class,'get_remita_config'])->name('get_remita_config');
-Route::get('check_pend_rrr',[RemitaConfig::class,'check_pend_rrr'])->name('check_pend_rrr');
-Route::post('log_new_rrr',[RemitaConfig::class,'log_new_rrr'])->name('log_new_rrr');
-Route::post('update_applicant_payment',[ApplicantPaymentController::class,'update_applicant_payment'])->name('update_applicant_payment');
-Route::get('profile',[AuthController::class,'user_profile'])->name('profile');
-Route::get('application',[AuthController::class,'view_applications'])->name('application');
-Route::get('payments',[AuthController::class,'view_payments'])->name('payments');
+    Route::get('get_remita_config',[RemitaConfig::class,'get_remita_config'])->name('get_remita_config');
+    Route::get('college_dept_prog',[ConfigController::class,'college_dept_prog'])->name('college_dept_prog');
+    Route::get('check_pend_rrr',[RemitaConfig::class,'check_pend_rrr'])->name('check_pend_rrr');
+    Route::post('log_new_rrr',[RemitaConfig::class,'log_new_rrr'])->name('log_new_rrr');
+    Route::post('update_applicant_payment',[ApplicantPaymentController::class,'update_applicant_payment'])->name('update_applicant_payment');
+    Route::get('profile',[AuthController::class,'user_profile'])->name('profile');
+    Route::get('application',[AuthController::class,'view_applications'])->name('application');
+    Route::get('payments',[AuthController::class,'view_payments'])->name('payments');
 
 
 
@@ -65,15 +66,3 @@ Route::get('payments',[AuthController::class,'view_payments'])->name('payments')
 // Route::post('save/app/form',[ApplicationController::class,'save_app_form'])->name('save.app.form');
 // Route::get('get_app_form',[ApplicationController::class,'get_app_form'])->name('get.app.form');
 // Route::get('create_application',[ApplicationController::class,'create_application'])->name('create.application');
-
-
-// Route::get('/profile', function () {
-    
-//     return view('/pages/profile');
-// });
-// Route::get('/payments', function () {
-//     return view('/pages/payment_history');
-// });
-// Route::get('/application', function () {
-//     return view('/pages/applications');
-// });
