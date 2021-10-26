20f54bf05febc369777125a893c51b1df1772d31
toyxy <toyxyboy@gmail.com>
2021-10-26 17:26:47 +0100

    update

---
+++ public/scripts/profile_update.js
@@ -0,0 +1,97 @@
+$(document).ready(function($){
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    })
+
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "5000",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+      }
+
+    $("#btn_pass").click(function(){ 
+        $("#update_password_form").validate({
+            submitHandler: submitFormPassword,
+            errorClass: "invalid"
+        });
+
+        function submitFormPassword(e) { 
+            var formData = $("#update_password_form").serialize();
+            var type = "POST";
+            var ajaxurl = 'password_reset';
+
+            $.ajax({
+                type: type,
+                url: ajaxurl,
+                data: formData,
+                dataType: 'json',
+                beforeSend: function() { 
+                    $("#btn_pass").html('<i class="fa fa-spinner fa-spin"></i> &nbsp; processing');
+                },
+                success: function (response) {
+                    console.log(response);
+                    $("#btn_pass").html('Update Password')
+                    toastr.options;
+                    toastr['success'](response);
+                },
+                error: function (response) {
+                    console.log(response);
+                    $("#btn_pass").html('Update Password');
+                    toastr.options;
+                    toastr['error'](response);
+                }
+            });
+
+        }
+    })
+
+    $("#btn_profile").click(function(){ 
+        $("#update_profile_form").validate({
+            submitHandler: submitFormProfile,
+            errorClass: "invalid"
+        });
+
+        function submitFormProfile(e) { 
+            var formData = $("#update_profile_form").serialize();
+            var type = "POST";
+            var ajaxurl = 'update_profile';
+
+            $.ajax({
+                type: type,
+                url: ajaxurl,
+                data: formData,
+                dataType: 'json',
+                beforeSend: function() { 
+                    $("#btn_profile").html('<i class="fa fa-spinner fa-spin"></i> &nbsp; processing');
+                },
+                success: function (response) {
+                    console.log(response);
+                    $("#btn_profile").html('Update Profile')
+                    toastr.options;
+                    toastr['success'](response);
+                },
+                error: function (response) {
+                    console.log(response);
+                    $("#btn_profile").html('Update Profile');
+                    toastr.options;
+                    toastr['error'](response);
+                }
+            });
+
+        }
+    })
+})
