f4946f34f81dae959a1262f4b0094234cee63b4f
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-05-27 11:25:14 +0100

    update

--- resources/views/admin/pages/pending_payments.blade.php
+++ resources/views/admin/pages/pending_payments.blade.php
@@ -66,7 +66,7 @@
                                                 <i class="bx bx-dots-horizontal-rounded"></i>
                                             </button>
                                             <ul class="dropdown-menu dropdown-menu-end">
-                                                <li><a class="dropdown-item" href="#">Approve</a></li>
+                                                <li><button data-id="$payment->id" data-rrr="$payment->rrr" data-email="$payment->email" class="dropdown-item approve" href="#">Approve</button></li>
                                             </ul>
                                         </div>
                                     </td>
@@ -96,9 +96,7 @@
         <script src="../assets_admin/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
 
         <!-- init js -->
-        <script src="../assets_admin/js/pages/datatable-pages.init.js"></script>
-        <script src="../assets_admin/js/pages/modal.init.js"></script>
-        
+        <script src="../assets_admin/js/pages/datatable-pages.init.js"></script>        
     @endsection
         
 
