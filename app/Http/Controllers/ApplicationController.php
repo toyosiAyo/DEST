ccdeb1357733bb5e8ada5825e31a5c48e4a495c2
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-05-27 13:28:08 +0100

    Update ApplicationController.php

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -49,9 +49,9 @@ class ApplicationController extends Controller
         try {
             $data = app('App\Http\Controllers\ConfigController')->auth_user(session('user'));
             $pin = $_COOKIE['pin'];
-            dd($pin);
             $app_type = $_COOKIE['app_type'];
             $form_status = DB::table('applications')->where(['submitted_by'=> $data->email,'app_type'=>$app_type,'status'=>'pending'])->pluck('form_status');
+            dd($form_status);
             if(!empty($pin) && !$form_status->isEmpty()){
                 $o_level = DB::table('o_level_subjects')->select('id','subject')->get();
                 $faculties = app('App\Http\Controllers\ConfigController')->college_dept_prog($request)['faculties'];
