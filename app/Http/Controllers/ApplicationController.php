26c07b696ae6682bfa6ae184dea85fc07c0e9a42
hameddavid <reganalyst@yahoo.com>
2022-06-21 17:14:52 +0100

    appication sumbmission update

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -142,7 +142,7 @@ class ApplicationController extends Controller
             $app->sponsor_email = $request->sponsor_email;
             $app->sponsor_phone = $request->sponsor_phone;
             if($app->save()){
-                $app_record = Application::where(['form_status' =>'0','submitted_by'=>$app->email,'status'=>'pending','app_type'=>$_COOKIE['app_type']])->first();
+                $app_record = Application::where(['submitted_by'=>$app->email,'status'=>'pending','app_type'=>$_COOKIE['app_type']])->first();
                 $app_record->form_status = $app_record->form_status+1;
                 dd($app_record);
                 if($app_record->save()){return response()->json(['status'=>'ok','msg'=>'success, profile created',],201); }
