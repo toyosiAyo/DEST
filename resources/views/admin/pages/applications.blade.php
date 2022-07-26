0f3b7fdc92a989be3988d612cb165a4bc7768ce7
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-07-26 14:45:03 +0100

    update

--- resources/views/admin/pages/applications.blade.php
+++ resources/views/admin/pages/applications.blade.php
@@ -74,7 +74,10 @@
                                                 <i class="bx bx-dots-horizontal-rounded"></i>
                                             </button>
                                             <ul class="dropdown-menu dropdown-menu-end">
-                                            @if($application->status == 'success')<li><a class="dropdown-item" href="#">Download</a></li>@endif
+                                            @if($application->status == 'success')
+                                                <li><a class="dropdown-item" href="#">Download</a></li>
+                                                <li><a class="dropdown-item" href="#">Approve</a></li>
+                                            @endif
                                             </ul>
                                         </div>
                                     </td>
