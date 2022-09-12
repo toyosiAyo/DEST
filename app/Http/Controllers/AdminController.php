4d691982b2a3661a968068c4aced164b27bf168b
hameddavid <reganalyst@yahoo.com>
2022-09-12 15:54:50 +0100

    adms let

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -19,7 +19,7 @@ class AdminController extends Controller
 {    
     public function __construct()
     {
-    //    $this->middleware('authcheck',['except' => ['login',]]);
+       $this->middleware('authcheck',['except' => ['login',]]);
        // $this->middleware('log')->only('index');
        // $this->middleware('subscribed')->except('store');
     }
@@ -41,7 +41,7 @@ class AdminController extends Controller
         // view / approve / download ... 
         $validator = Validator::make($request->all(), ['email'=>'required|email','app_id'=>'required','action'=>'required',]);
         if ($validator->fails()) { return response()->json(['status'=>'Nok','msg'=>'Email/app_id/action are required','rsp'=>''], 401);        } 
-        // $data = app('App\Http\Controllers\ConfigController')->adminUser(session('user'));
+        $data = app('App\Http\Controllers\ConfigController')->adminUser(session('user'));
         $get_app = Application::join('applicants','applications.submitted_by','applicants.email')
          ->where(['applications.id'=>$request->app_id,'applications.submitted_by'=>$request->email]) 
          ->select('applications.first_choice->prog as Programme1','applications.second_choice->prog as Programme2','applicants.*','applications.*')->first();
