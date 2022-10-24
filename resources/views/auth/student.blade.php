66e7aed00094e1cf383834fdbd3149ebaf3e7f3a
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-10-24 02:14:23 +0100

    Update student.blade.php

--- resources/views/auth/student.blade.php
+++ resources/views/auth/student.blade.php
@@ -7,6 +7,7 @@
   @push('head')
   <link rel="stylesheet" href="{{ asset('assets/examples/css/pages/login-v4.minfd53.css?v4.0.1') }}">
   <link rel="stylesheet" href="{{ asset('assets/examples/css/pages/login-v2.minfd53.css?v4.0.1') }}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   @endpush
 
   @section("content")
@@ -52,7 +53,7 @@
         
         </div>
       </div>
-      
+
       <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
       <script src=" {{ asset('scripts/validation.min.js') }}"></script>
       <script src=" {{ asset('scripts/student_auth.js') }}"></script>
