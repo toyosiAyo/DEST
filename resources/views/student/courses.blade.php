ade9944a46c8d7026cf5774a083edc58a90af22f
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-10-26 12:02:21 +0100

    Update courses.blade.php

--- resources/views/student/courses.blade.php
+++ resources/views/student/courses.blade.php
@@ -14,7 +14,7 @@
                             <div class="panel-heading">
                                 <h3 class="panel-title">Registered Courses</h3>
                             </div>
-                            <div class="table-responsive h-250">
+                            <div class="table-responsive">
                                 <div data-role="container">
                                     <div data-role="content">
                                         <table id="reg_table" class="table table-responsive-sm table-hover table-striped">
