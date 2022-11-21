20d834cc56601132472a8aef7b70e9f061a157e4
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2022-11-21 12:44:52 +0100

    update

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -24,10 +24,11 @@ class StudentController extends Controller
                 return response()->json(['message'=>'failed', 'info'=>'programme/degree is required!']);
             }   
             $data = app('App\Http\Controllers\ConfigController')->auth_user(session('user'));
+            $setting = app('App\Http\Controllers\ConfigController')->settings($request);
             $prog = DB::table('programmes')->where('programme',$_COOKIE['prog_id'])->first();  
             $courses = DB::table('curriculum')->join('courses', 'curriculum.course_code', '=', 'courses.course_code')
                 ->where([['curriculum.degree',$_COOKIE['degree']],
-                    ['curriculum.semester',1],['curriculum.year',1],['curriculum.programme_id',$prog->programme_id]])
+                    ['curriculum.semester',$setting->semester_code],['curriculum.year',1],['curriculum.programme_id',$prog->programme_id]])
                 ->select('curriculum.*','courses.unit','courses.course_title')->get();
             //$registered = $this->viewRegisteredCourses($request);
             return view('student.registration',['courses'=>$courses,'data'=>$data]);
@@ -39,8 +40,9 @@ class StudentController extends Controller
 
     public function view_registered_courses(Request $request){
         $data = app('App\Http\Controllers\ConfigController')->auth_user(session('user'));
+        $setting = app('App\Http\Controllers\ConfigController')->settings($request);
         $courses = DB::table('registration')->join('courses', 'registration.course_code', '=', 'courses.course_code')
-            ->where([['student_id', $data->id],['settings_id', '3']])->select('courses.*','registration.unit')->get();
+            ->where([['student_id', $data->id],['settings_id', $setting->id]])->select('courses.*','registration.unit')->get();
         return view('student.courses',['courses'=>$courses,'data'=>$data]);
     }
 
@@ -48,6 +50,7 @@ class StudentController extends Controller
         $request->validate([ 'course' => 'required']);
         try {
             $data = app('App\Http\Controllers\ConfigController')->auth_user(session('user'));
+            $setting = app('App\Http\Controllers\ConfigController')->settings($request);
             $check = DB::table('registration')->where(['student_id'=> $data->id,'settings_id'=>'3'])->first();
                 if($check){
                     return response()->json(['status'=>'Nok','message'=>'Registration already submitted for approval',],401); 
@@ -57,7 +60,7 @@ class StudentController extends Controller
                 DB::table('registration')->insert([
                     'course_code' => $course[0],
                     'student_id' => $data->id,
-                    'settings_id' => 3,
+                    'settings_id' => $setting->id,
                     'unit' => $course[2]
                 ]);
             }
