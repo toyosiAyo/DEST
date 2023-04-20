7f1bf48901eff595f061acf5f9f14539f9b07bef
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2023-04-20 13:34:44 +0100

    Update lecturers.blade.php

--- resources/views/admin/pages/lecturers.blade.php
+++ resources/views/admin/pages/lecturers.blade.php
@@ -124,6 +124,7 @@
 
         <!-- init js -->
         <script src="../assets_admin/js/pages/datatable-pages.init.js"></script>
+        <script src="../assets_admin/js/pages/modal.init.js"></script>
         <script src="../assets_admin/scripts/utility.js"></script>
         
     @endsection
