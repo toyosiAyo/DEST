d63cfbe168f421b8b876afd205d445cc69a525e1
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2023-06-12 23:25:48 +0100

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -626,7 +626,7 @@ class AdminController extends Controller
         //dd(collect($regs)->collapse()->all());
  
         $curriculum = DB::table('curriculum')->where(['course_status'=>'C','semester'=>$request->semester,'programme_id'=>$programme])
-        ->whereIn('course_code', $regs)->pluck('course_code');
+        ->whereIn('course_code', $regs)->get();
         dd($curriculum);
 
         $failed_courses = DB::table('registration')->where(['student_id'=>$value->student_id])
