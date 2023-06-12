02c7c781eac9dddbff8cf455a9b59073b6cd34a6
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2023-06-12 23:34:11 +0100

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -643,32 +643,32 @@ class AdminController extends Controller
         }
         $cgpa = round($cgpa,2);
         if($cgpa >= 4.50){
-            $class_performance_summary['first_class'] = int($class_performance_summary['first_class']+1);
+            $class_performance_summary['first_class'] = intval($class_performance_summary['first_class']+1);
             return 'GSD (Excellent)';
         }
             
         elseif($cgpa >= 3.50 && $cgpa <= 4.49) {
-            $class_performance_summary['second_class_upper'] = int($class_performance_summary['second_class_upper']+1);
+            $class_performance_summary['second_class_upper'] = intval($class_performance_summary['second_class_upper']+1);
             return 'GSD (V. Good)';
         } 
             
         elseif($cgpa >= 2.50 && $cgpa <= 3.49){
-            $class_performance_summary['second_class_lower'] = int($class_performance_summary['second_class_lower']+1);
+            $class_performance_summary['second_class_lower'] = intval($class_performance_summary['second_class_lower']+1);
             return 'GSD (Good)';
         } 
             
         elseif($cgpa >= 1.50 && $cgpa <= 2.49){
-            $class_performance_summary['third_class'] = int($class_performance_summary['third_class']+1);
+            $class_performance_summary['third_class'] = intval($class_performance_summary['third_class']+1);
             return 'GSD (Average)';
         }  
             
         elseif($cgpa >= 1.00 && $cgpa <= 1.49){
-            $class_performance_summary['pass'] = int($class_performance_summary['pass']+1);
+            $class_performance_summary['pass'] = intval($class_performance_summary['pass']+1);
             return 'PRB (Fair)';
         } 
             
         elseif($cgpa < 1.00 ){
-            $class_performance_summary['poor'] = int($class_performance_summary['poor']+1);
+            $class_performance_summary['poor'] = intval($class_performance_summary['poor']+1);
             return 'WRN (V. Poor)';
         }
         else{
@@ -736,7 +736,7 @@ class AdminController extends Controller
 
     public function getGPA($tnu,$tcp){
         if($tnu != 0 && $tnu != '' && $tcp = ''){
-            return round(int($tcp)/int($tnu),2);
+            return round(intval($tcp)/intval($tnu),2);
         }
         return '';
     }
