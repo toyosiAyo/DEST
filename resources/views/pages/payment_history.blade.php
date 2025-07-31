45fc33eba49f169322fa96d1b7b0d529e3fa9212
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2025-07-31 18:26:41 +0100

    Update payment_history.blade.php

--- resources/views/pages/payment_history.blade.php
+++ resources/views/pages/payment_history.blade.php
@@ -1,10 +1,11 @@
-@extends("layouts.master") 
+@extends('layouts.master')
 
-  @section("title")
-      Payment History
-  @endsection
+@section('title')
+    Payment History
+@endsection
 
-  @section("content")
+@section('content')
+
     <body class="animsition site-menubar-push site-menubar-open site-menubar-fixed">
         <div class="page">
             <div class="page-content container-fluid">
@@ -18,7 +19,8 @@
                             <div class="table-responsive h-250" data-plugin="scrollable">
                                 <div data-role="container">
                                     <div data-role="content">
-                                        <table class="table table-responsive-sm table-hover table-striped">
+                                        <table id="payment_history"
+                                            class="table table-responsive-sm table-hover table-striped">
                                             <thead>
                                                 <tr>
                                                     <th>S/N</th>
@@ -31,25 +33,34 @@
                                             </thead>
                                             <tbody>
                                                 @php $i = 1; @endphp
-                                                @foreach($payments as $payment)
-                                                <tr>
-                                                    <td>{{ $i }} @php $i++ @endphp</td>
-                                                    <td>{{ $payment->trans_ref }}</td>
-                                                    <td>{{ $payment->amount }}</td>
-                                                    <td>{{ date("d M Y", strtotime($payment->updated_at)) }}</td>
-                                                    <td>@php echo $payment->status_msg == 'pending' ?
-                                                        '<span class="badge badge-warning"> '.$payment->status_msg.'</span>' :
-                                                        '<span class="badge badge-success">'.$payment->status_msg.'</span>' @endphp
-                                                    </td>
-                                                    <td>@php echo $payment->status_msg == 'pending' ?
-                                                        '<button type="button" class="btn btn-danger">
-                                                            <i class="icon md-refresh" aria-hidden="true"></i> Requery
-                                                        </button>' :
-                                                        '<a href="receipt/'.$payment->trans_ref.'" target="_blank" type="button" class="btn btn-success">
-                                                            <i class="icon md-print" aria-hidden="true"></i> Print Receipt
-                                                        </a>' @endphp
-                                                    </td>
-                                                </tr>
+                                                @foreach ($payments as $payment)
+                                                    <tr>
+                                                        <td>{{ $i }} @php $i++ @endphp</td>
+                                                        <td>{{ $payment->trans_ref }}</td>
+                                                        <td>{{ $payment->amount }}</td>
+                                                        <td>{{ date('d M Y', strtotime($payment->updated_at)) }}</td>
+                                                        <td>@php echo $payment->status_msg == 'pending' ? '<span class="badge badge-warning"> ' . $payment->status_msg . '</span>' : '<span class="badge badge-success">' . $payment->status_msg . '</span>'; @endphp
+                                                        </td>
+                                                        <td>
+                                                            @if ($payment->status_msg == 'success')
+                                                                <a href="receipt/' .
+                                                                    $payment->trans_ref .
+                                                                '"
+                                                                    target="_blank" type="button" class="btn btn-success">
+                                                                    <i class="icon md-print" aria-hidden="true"></i> Print
+                                                                    Receipt
+                                                                </a>
+                                                            @else
+                                                                <button type="button"
+                                                                    data-reference="{{ $payment->trans_ref }}"
+                                                                    data-amount="{{ $payment->amount }}"
+                                                                    class="btn btn-danger requery">
+                                                                    <i class="icon md-refresh" aria-hidden="true"></i>
+                                                                    Requery
+                                                                </button>
+                                                            @endif
+                                                        </td>
+                                                    </tr>
                                                 @endforeach
                                             </tbody>
                                         </table>
@@ -63,7 +74,5 @@
             </div>
         </div>
     </body>
-
-
-
-  @endsection
+    <script src=" {{ asset('scripts/payment.js') }}"></script>
+@endsection
