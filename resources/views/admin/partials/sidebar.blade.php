0d1d83f7251db4d20be5b93fe24d0c701fd85f05
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2025-08-21 01:47:00 +0100

    Update sidebar.blade.php

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -7,89 +7,90 @@
             <!-- Left Menu Start -->
             <ul class="metismenu list-unstyled" id="side-menu">
                 <li class="menu-title">Menu</li>
-                @if($data->role == 'director' || $data->role == 'admin' || $data->role == 'accountant')
-                <li>
-                    <a href="dashboard">
-                        <i data-feather="home"></i>
-                        <span>Dashboard</span>
-                    </a>
-                </li>
+                @if ($data->role == 'director' || $data->role == 'admin' || $data->role == 'accountant')
+                    <li>
+                        <a href="dashboard">
+                            <i data-feather="home"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
                 @endif
-                @if($data->role == 'director' || $data->role == 'admin')
-                <li>
-                    <a href="{{route('viewApplicants')}}">
-                        <i data-feather="users"></i>
-                        <span>Applicants</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/admin/students">
-                        <i data-feather="users"></i>
-                        <span>Students</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{route('view_lecturers')}}">
-                        <i data-feather="user-plus"></i>
-                        <span>Lecturers</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow">
-                        <i data-feather="file-text"></i>
-                        <span>Applications</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="{{route('viewApplications')}}">All Applications</a></li>
-                        <li><a href="{{route('viewPendingApplications')}}">Pending Applications</a></li>
-                    </ul>
-                </li>
+                @if ($data->role == 'director' || $data->role == 'admin')
+                    <li>
+                        <a href="{{ route('viewApplicants') }}">
+                            <i data-feather="users"></i>
+                            <span>Applicants</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/admin/students">
+                            <i data-feather="users"></i>
+                            <span>Students</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('view_lecturers') }}">
+                            <i data-feather="user-plus"></i>
+                            <span>Lecturers</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow">
+                            <i data-feather="file-text"></i>
+                            <span>Applications</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            <li><a href="{{ route('viewApplications') }}">All Applications</a></li>
+                            <li><a href="{{ route('viewSubmittedApplications') }}">Submitted Applications</a></li>
+                            <li><a href="{{ route('viewPendingApplications') }}">Pending Applications</a></li>
+                        </ul>
+                    </li>
                 @endif
-                @if($data->role == 'director' || $data->role == 'admin' || $data->role == 'accountant')
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow">
-                        <i class="fas fa-hand-holding-usd"></i>
-                        <span>Payments</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="{{route('allpayments')}}">All Payments</a></li>
-                        <li><a href="{{route('pending_payments')}}">Pending</a></li>
-                    </ul>
-                </li>
+                @if ($data->role == 'director' || $data->role == 'admin' || $data->role == 'accountant')
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow">
+                            <i class="fas fa-hand-holding-usd"></i>
+                            <span>Payments</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            <li><a href="{{ route('allpayments') }}">All Payments</a></li>
+                            <li><a href="{{ route('pending_payments') }}">Pending</a></li>
+                        </ul>
+                    </li>
                 @endif
-                @if($data->role == 'director' || $data->role == 'admin')
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow">
-                        <i class="fab fa-whmcs"></i>
-                        <span>Config</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="#">Create Course</a></li>
-                        <li><a href="{{route('curriculum')}}">Create Curriculum</a></li>
-                        <li><a href="{{route('view_curriculum')}}">View Curriculum</a></li>
-                        <li><a href="{{route('events')}}">Post News/Events</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="{{route('view_registration')}}">
-                        <i data-feather="file-text"></i>
-                        <span>Course Registration</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{route('view_results')}}">
-                        <i data-feather="layers"></i>
-                        <span>Results</span>
-                    </a>
-                </li>
+                @if ($data->role == 'director' || $data->role == 'admin')
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow">
+                            <i class="fab fa-whmcs"></i>
+                            <span>Config</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            <li><a href="#">Create Course</a></li>
+                            <li><a href="{{ route('curriculum') }}">Create Curriculum</a></li>
+                            <li><a href="{{ route('view_curriculum') }}">View Curriculum</a></li>
+                            <li><a href="{{ route('events') }}">Post News/Events</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="{{ route('view_registration') }}">
+                            <i data-feather="file-text"></i>
+                            <span>Course Registration</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('view_results') }}">
+                            <i data-feather="layers"></i>
+                            <span>Results</span>
+                        </a>
+                    </li>
                 @endif
-                @if($data->role !== 'director' && $data->role !== 'admin' && $data->role !== 'accountant')
-                <li>
-                    <a href="{{route('score_input')}}">
-                        <i data-feather="edit"></i>
-                        <span>Score Input</span>
-                    </a>
-                </li>
+                @if ($data->role !== 'director' && $data->role !== 'admin' && $data->role !== 'accountant')
+                    <li>
+                        <a href="{{ route('score_input') }}">
+                            <i data-feather="edit"></i>
+                            <span>Score Input</span>
+                        </a>
+                    </li>
                 @endif
             </ul>
         </div>
