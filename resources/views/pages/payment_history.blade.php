b0476136e35cc9ef08038b3ec10f3bf9802e2125
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2025-09-23 18:06:31 +0100

    Update payment_history.blade.php

--- resources/views/pages/payment_history.blade.php
+++ resources/views/pages/payment_history.blade.php
@@ -14,7 +14,7 @@
                         <!-- Panel Tasks -->
                         <div class="panel">
                             <div class="panel-heading">
-                                <h3 class="panel-title">Payment History</h3>
+                                <h3 class="panel-title">Application Payment History</h3>
                             </div>
                             <div class="table-responsive h-250" data-plugin="scrollable">
                                 <div data-role="container">
@@ -69,6 +69,63 @@
                             </div>
                         </div>
                         <!-- End Panel Tasks -->
+
+                        <div class="panel">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">Admission Payment History</h3>
+                            </div>
+                            <div class="table-responsive h-250" data-plugin="scrollable">
+                                <div data-role="container">
+                                    <div data-role="content">
+                                        <table id="payment_history_admission"
+                                            class="table table-responsive-sm table-hover table-striped">
+                                            <thead>
+                                                <tr>
+                                                    <th>S/N</th>
+                                                    <th>Transaction ID</th>
+                                                    <th>Amount</th>
+                                                    <th>Date</th>
+                                                    <th>Status</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @php $i = 1; @endphp
+                                                @foreach ($admission_payments as $payment)
+                                                    <tr>
+                                                        <td>{{ $i }} @php $i++ @endphp</td>
+                                                        <td>{{ $payment->trans_ref }}</td>
+                                                        <td>{{ $payment->amount }}</td>
+                                                        <td>{{ date('d M Y', strtotime($payment->updated_at)) }}</td>
+                                                        <td>@php echo $payment->status == 'pending' ? '<span class="badge badge-warning"> ' . $payment->status . '</span>' : '<span class="badge badge-success">' . $payment->status . '</span>'; @endphp
+                                                        </td>
+                                                        <td>
+                                                            @if ($payment->status == 'success')
+                                                                <a href="receipt/' .
+                                                                    $payment->trans_ref .
+                                                                '"
+                                                                    target="_blank" type="button" class="btn btn-success">
+                                                                    <i class="icon md-print" aria-hidden="true"></i> Print
+                                                                    Receipt
+                                                                </a>
+                                                            @else
+                                                                <button type="button"
+                                                                    data-reference="{{ $payment->trans_ref }}"
+                                                                    data-amount="{{ $payment->amount }}"
+                                                                    class="btn btn-danger requery_admission_payment">
+                                                                    <i class="icon md-refresh" aria-hidden="true"></i>
+                                                                    Requery
+                                                                </button>
+                                                            @endif
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
