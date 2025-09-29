79f178092b88c89a4313e1ea24e84a89b8316e77
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2025-09-29 11:34:02 +0100

    Update BulkEmailMail.php

--- app/Mail/BulkEmailMail.php
+++ app/Mail/BulkEmailMail.php
@@ -12,21 +12,27 @@ class BulkEmailMail extends Mailable
     use Queueable, SerializesModels;
 
     public $name;
+    public $id;
     public $data;
 
-    public function __construct($name,$data)
+    public function __construct($name,$id,$data)
     {
         $this->name = $name;
+        $this->id = $id;
         $this->data = $data;
     }
 
     public function build()
     {
+        if($this->data["type"]=="screening"){
+            $screening_message = "Your exam login details are as follows:<br>Username: ".$this->id."<br>Password: ".$this->data["message"];
+            $this->data["message"] = $screening_message;
+        }
         $this->data["name"] = $this->name;
         return $this->subject($this->data["subject"])
-                    ->view('emails.notify_student')
-                    ->with([
-                        'data' => $this->data,
-                    ]);
+            ->view('emails.notify_student')
+            ->with([
+                'data' => $this->data,
+            ]);
     }
 }
