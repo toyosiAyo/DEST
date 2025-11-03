250f18c2edc78dbfa27f5f6e4fd55f83b5276c66
toyosiAyo <80500110+toyosiAyo@users.noreply.github.com>
2025-11-03 01:22:44 +0100

    Create OpenApiInfo.php

---
+++ app/Swagger/OpenApiInfo.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * OpenAPI Info
+ *
+ * @OA\Info(
+ *   version="1.0.0",
+ *   title="DEST API",
+ *   description="API documentation for DEST application"
+ * )
+ */
+
+// This file only contains OpenAPI annotations for L5-Swagger to discover.
